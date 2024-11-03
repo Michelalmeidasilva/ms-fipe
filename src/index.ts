95fe8edd6e750876474ec5b1e0b0e211b958c9cd
User <user@MacBook-Pro-de-User.local>
2024-11-03 02:34:25 -0300

    feat: added memcached

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,15 @@ var express = require('express');
 var { createHandler } = require('graphql-http/lib/use/express');
 var { buildSchema } = require('graphql');
 
+var Memcached = require('memcached');
+
+var memcached = new Memcached('localhost:11211', {
+  retries: 10,
+  retry: 10000,
+  remove: true,
+  failOverServers: ['192.168.0.103:11211'],
+});
+
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
